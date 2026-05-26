117f43c1857c8ebfd68441a28e2b98353a3f3fc8
iwangjunhui-byte <iwangjunhui@gmail.com>
2026-05-26 18:50:14 +0800

    Add Spring Boot application entry

---
+++ src/main/java/com/iwangjunhui/AutoRegistApplication.java
@@ -0,0 +1,12 @@
+package com.iwangjunhui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRegistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRegistApplication.class, args);
+    }
+}
